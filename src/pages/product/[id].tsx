9cd71791b7e38dfa0f17e79a7e03c0fc4838c97b
lucastheldl <lucastheldl@gmail.com>
2023-09-08 16:30:42 -0300

    feat: improved SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import {
 } from "@/styles/pages/product";
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -49,21 +50,30 @@ export default function Product({ product }: ProductProps) {
     return <p>Loading...</p>;
   }
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
 
-      <DetailsContainer>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
 
-        <p>{product.description}</p>
-        <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
-          Comprar agora
-        </button>
-      </DetailsContainer>
-    </ProductContainer>
+        <DetailsContainer>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+          <button
+            onClick={handleBuyProduct}
+            disabled={isCreatingCheckoutSession}
+          >
+            Comprar agora
+          </button>
+        </DetailsContainer>
+      </ProductContainer>
+    </>
   );
 }
 export const getStaticPaths: GetStaticPaths = async () => {
